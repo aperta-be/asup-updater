68ae10c634ec545d8474560dc6175ef35ca6a9c5
nstefanis <n.stefanis@gmail.com>
2022-03-04 02:17:36 +0200

    Small fixes

--- scripts/app/sh/ssh.sh
+++ scripts/app/sh/ssh.sh
@@ -5,7 +5,7 @@ function ssh_keys_install() {
   mkdir -p ~/.ssh
 
   # If no public/private keys are provided, use the default ones.
-  if [ $SSH_PUBLIC_KEY == 0 ] && [ $SSH_PRIVATE_KEY == 0 ]; then
+  if [ "$SSH_PUBLIC_KEY" == 0 ] && [ "$SSH_PRIVATE_KEY" == 0 ]; then
     mv /mount/ssh/* ~/.ssh/
   else
     echo "$SSH_PUBLIC_KEY" | tr -d '\r' > ~/.ssh/id_asup.pub
@@ -13,11 +13,10 @@ function ssh_keys_install() {
   fi
 
   chmod 600 ~/.ssh/id_asup
-  eval `ssh-agent -s`
+  eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_asup
 }
 
 function ssh_keys_validate() {
-  #ssh-keyscan -H $ssh_port "$ssh_host" >> ~/.ssh/known_hosts
-  ssh-keyscan -H 22 "gitlab.dazzle.be" >> ~/.ssh/known_hosts
+  ssh-keyscan -H $GITLAB_HOST >> ~/.ssh/known_hosts
 }
