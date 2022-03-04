c9e8becdfd023cdd46a0d651de31c9798562a239
nstefanis <n.stefanis@gmail.com>
2022-03-04 02:13:20 +0200

    Inherit variables from gitlab CI environment

--- scripts/app/sh/prerequisites.sh
+++ scripts/app/sh/prerequisites.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 
 # Check or set these values.
-if ! [ -v ${GIT_AUTO_MERGE+x} ]; then echo "GIT_AUTO_MERGE provided with value: $GIT_AUTO_MERGE"; else GIT_AUTO_MERGE=0; fi
-if ! [ -v ${GITLAB_HOST+x} ]; then echo "GITLAB_HOST provided with value: $GITLAB_HOST"; else GITLAB_HOST="https://gitlab.dazzle.be"; fi
-if ! [ -v ${GITLAB_TOKEN+x} ]; then echo "GITLAB_TOKEN provided with value: $GITLAB_TOKEN"; else echo -e "# \e[1;31mGITLAB_TOKEN not provided.\e[0m"; exit 1; fi
-if ! [ -v ${GITLAB_PROJECT_ID+x} ]; then echo "GITLAB_PROJECT_ID provided with value: $GITLAB_PROJECT_ID"; else echo -e "# \e[1;31mGITLAB_PROJECT_ID not provided.\e[0m"; exit 1; fi
-if ! [ -v ${GIT_BRANCH_TARGET+x} ]; then echo "GIT_BRANCH_TARGET provided with value: $GIT_BRANCH_TARGET"; else echo -e "# \e[1;31mGIT_BRANCH_TARGET not provided.\e[0m"; exit 1; fi
-if ! [ -v ${GIT_CLONE_URL+x} ]; then echo "GIT_CLONE_URL provided with value: $GIT_CLONE_URL"; else echo -e "# \e[1;31mGIT_CLONE_URL not provided.\e[0m"; exit 1; fi
 if ! [ -v ${COMPOSER_UPDATE_CONSTRAINTS+x} ]; then echo "COMPOSER_UPDATE_CONSTRAINTS provided with value: $COMPOSER_UPDATE_CONSTRAINTS"; else echo -e "# \e[1;31mCOMPOSER_UPDATE_CONSTRAINTS not provided. Assuming 0.\e[0m"; COMPOSER_UPDATE_CONSTRAINTS=0; fi
+if ! [ -v ${GIT_AUTO_MERGE+x} ]; then echo "GIT_AUTO_MERGE provided with value: $GIT_AUTO_MERGE"; else GIT_AUTO_MERGE=0; fi
+
+if ! [ -v ${GITLAB_HOST+x} ]; then echo "GITLAB_HOST provided with value: $GITLAB_HOST"
+  else if ! [ -v ${CI_SERVER_HOST+x} ]; then
+    GITLAB_HOST=$CI_SERVER_HOST
+    echo "GITLAB_HOST provided by gitlab CI with value: $CI_SERVER_HOST"
+  else GITLAB_HOST="gitlab.dazzle.be"
+  fi
+fi
+
+if ! [ -v ${GITLAB_PROJECT_ID+x} ]; then echo "GITLAB_PROJECT_ID provided with value: $GITLAB_PROJECT_ID"
+  else if ! [ -v ${CI_PROJECT_PATH+x} ]; then
+    GITLAB_PROJECT_ID=$CI_PROJECT_PATH
+    echo "GITLAB_PROJECT_ID provided by gitlab CI with value: $CI_PROJECT_PATH"
+  else echo -e "# \e[1;31mGITLAB_PROJECT_ID not provided.\e[0m"; exit 1;
+  fi
+fi
+
+if ! [ -v ${GIT_BRANCH_TARGET+x} ]; then echo "GIT_BRANCH_TARGET provided with value: $GIT_BRANCH_TARGET"
+  else if ! [ -v ${CI_COMMIT_BRANCH+x} ]; then
+    GIT_BRANCH_TARGET=$CI_COMMIT_BRANCH
+    echo "GIT_BRANCH_TARGET provided by gitlab CI with value: $CI_COMMIT_BRANCH"
+  else echo -e "# \e[1;31mGIT_BRANCH_TARGET not provided.\e[0m"; exit 1
+  fi
+fi
+
+if ! [ -v ${GIT_CLONE_URL+x} ]; then echo "GIT_CLONE_URL provided with value: $GIT_CLONE_URL"
+  else if ! [ -v ${CI_SERVER_HOST+x} ] && ! [ -v ${CI_PROJECT_PATH+x} ]; then
+    GIT_CLONE_URL="git@"$CI_SERVER_HOST":"$CI_PROJECT_PATH".git"
+    echo "GIT_CLONE_URL was built by gitlab CI with value: $GIT_CLONE_URL"
+  else echo -e "# \e[1;31mGIT_BRANCH_TARGET not provided.\e[0m"; exit 1
+  fi
+fi
+
+if ! [ -v ${GITLAB_TOKEN+x} ]; then echo "GITLAB_TOKEN provided with value: $GITLAB_TOKEN"; else echo -e "# \e[1;31mGITLAB_TOKEN not provided.\e[0m"; exit 1; fi
 if ! [ -v ${SSH_PUBLIC_KEY+x} ]; then echo "SSH_PUBLIC_KEY provided with value: $SSH_PUBLIC_KEY"; else echo -e "# \e[1;31mSSH_PUBLIC_KEY not provided. Assuming default.\e[0m"; SSH_PUBLIC_KEY=0; fi
 if ! [ -v ${SSH_PRIVATE_KEY+x} ]; then echo "SSH_PRIVATE_KEY provided, but not printing here for security reasons."; else echo -e "# \e[1;31mSSH_PRIVATE_KEY not provided. Assuming default.\e[0m"; SSH_PRIVATE_KEY=0; fi
 
 if ! [ -v ${APP_PUBLIC_ROOT_DIRECTORY+x} ]; then
-  echo "APP_PUBLIC_ROOT_DIRECTORY provided with value: $APP_PUBLIC_ROOT_DIRECTORY";
+  if [[ $APP_PUBLIC_ROOT_DIRECTORY == 0 ]]; then
+    echo "APP_PUBLIC_ROOT_DIRECTORY (0) default to GIT root: $APP_CODE_DIRECTORY";
+    APP_PUBLIC_ROOT_DIRECTORY=$APP_CODE_DIRECTORY;
+    else
+      echo "APP_PUBLIC_ROOT_DIRECTORY provided with value: $APP_PUBLIC_ROOT_DIRECTORY";
+      APP_PUBLIC_ROOT_DIRECTORY=$APP_CODE_DIRECTORY/$APP_PUBLIC_ROOT_DIRECTORY;
+  fi
   else echo -e "# \e[1;31mAPP_PUBLIC_ROOT_DIRECTORY not provided. Assuming web.\e[0m"; APP_PUBLIC_ROOT_DIRECTORY="web";
-  # Finetune APP_PUBLIC_ROOT_DIRECTORY if needed to not work with a subdirectory.
-  if [[ $APP_PUBLIC_ROOT_DIRECTORY == 0 ]]; then APP_PUBLIC_ROOT_DIRECTORY=$APP_CODE_DIRECTORY/$APP_PUBLIC_ROOT_DIRECTORY; else APP_PUBLIC_ROOT_DIRECTORY=$APP_CODE_DIRECTORY; fi
 fi
 
 # Output the version of software we need. This will fail if not installed and it is what we want.
