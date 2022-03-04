28707938941c17b9e65f2a11ea1fb71cbfc11542
nstefanis <n.stefanis@gmail.com>
2022-03-04 02:08:09 +0200

    Fix .env parsing for spaces and newlines

--- scripts/app/sh/check_env.sh
+++ scripts/app/sh/check_env.sh
@@ -3,5 +3,6 @@
 # Here we have the ability to set some variables via a local .env file. This is useful for development.
 if [ -f /usr/local/bin/.env ]
 then
-  export $(cat /usr/local/bin/.env | sed 's/#.*//g' | xargs)
+  echo -e "# \e[1;31mCaution this docker image compiled with .env file and all variables that exist on .env are static. Do not push to gitlab.\e[0m";
+  set -o allexport; source /usr/local/bin/.env; set +o allexport
 fi
