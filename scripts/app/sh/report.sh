9d7c59437aba10fe3cd0a47ea70c68c0f11c0555
Nicolas Stefanis <n.stefanis@gmail.com>
2022-03-14 17:32:24 +0200

    Fix reg-ex to include beta, alpha and dev versions

--- scripts/app/sh/report.sh
+++ scripts/app/sh/report.sh
@@ -16,7 +16,7 @@ function report_mattermost() {
     echo "OUTDATED_FILE is not empty"
     if [[ $COMPOSER_UPDATE_CONSTRAINTS != "1" ]]; then REPORT="${REPORT}:warning: Update constraints is not enabled.\n"; fi
     OUTDATED_TABLE="\n\n\n| Modules | Version | \n|:----------------|:---------------|\n"
-    OUTDATED_TABLE="${OUTDATED_TABLE} $(echo "${OUTDATED_FILE}" | sed -r 's/([a-z_-]+\/[a-z_-]+).* [v]?([0-9]+\.[0-9]+\.[0-9]+).* [v]?([0-9]+\.[0-9]+\.[0-9]+) .*$/| \1 | \2 > \3 |/g')"
+    OUTDATED_TABLE="${OUTDATED_TABLE} $(echo "${OUTDATED_FILE}" | sed -r 's/([a-z_-]+\/[a-z_-]+).* [v]?([0-9]+\.[0-9]+\.[0-9]+(-beta[0-9]+|-alpha[0-9]+|-dev[0-9]+)?).* [v]?([0-9]+\.[0-9]+\.[0-9]+(-beta[0-9]+|-alpha[0-9]+|-dev[0-9]+)?).*$/| \1 | \2 > \4 |/g')"
   fi
 
   curl -i -X POST -H 'Content-Type: application/json' -d "{\"username\": \"ASUP\", \"text\": \"$REPORT $OUTDATED_TABLE\"}" "$MATTERMOST_HOOK"
