77531541e27f1acdaf9e7f25944c06b032a9f6b5
Kevin Thiels <kevin.thiels@dazzle.be>
2023-01-13 20:23:14 +0100

    Issue: Testing

--- ci-factory.sh
+++ ci-factory.sh
@@ -2,6 +2,7 @@
 
 # If no release tag provided fallback to 0.0
 [ -z "$1" ] && export RELEASE="0.0" || RELEASE="$1"
+[ -z "$2" ] && export DEV="" || DEV=$2
 
 for PHP_VERSION in $(cat php_versions); do
   # Check if a dedicated Dockerfile exist.
@@ -12,11 +13,13 @@ for PHP_VERSION in $(cat php_versions); do
 
   cat .gitlab-ci-child-build-template.yml |
     sed "s/<PHP_VERSION>/${PHP_VERSION}/g" |
+    sed "s/<DEV>/${DEV}/g" |
     sed "s/<TAG>/${RELEASE}/g" |
     sed "s/<DOCKERFILE>/${DOCKERFILE}/g" >> .gitlab-ci-child-build-dynamic.yml
 
   cat .gitlab-ci-child-test-template.yml |
     sed "s/<PHP_VERSION>/${PHP_VERSION}/g" |
+    sed "s/<DEV>/${DEV}/g" |
     sed "s/<TAG>/${RELEASE}/g" |
     sed "s/<DOCKERFILE>/${DOCKERFILE}/g" >> .gitlab-ci-child-test-dynamic.yml
 
