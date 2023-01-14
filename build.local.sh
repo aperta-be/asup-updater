5b8b6185e68bbabcaeb23d78bc614f08ad4b6bd3
Kevin Thiels <kevin.thiels@dazzle.be>
2023-01-14 20:53:09 +0100

    Issue: Switch to composer-outdated-project
    The Drupal project is too tight to a specific version and is conflicting thus always failing.

--- build.local.sh
+++ build.local.sh
@@ -6,6 +6,12 @@ MATTERMOST_HOOK="https://mattermost.dazzle.be/hooks/f8rb7d4fufnu3m4qbcdrauhuge"
 source build.sh
 
 for v in $(cat php_versions); do
+  # Check if a dedicated Dockerfile exist.
+    DOCKERFILE="Dockerfile"
+    if test -f "Dockerfile-${v}"; then
+      DOCKERFILE="Dockerfile-${v}"
+      echo Using specific Dockerfile "Dockerfile-${v}"
+    fi
   echo Using docker build with "-t asup:$v --build-arg PHP_VERSION=$v"
-  docker build -t asup:dev-$v --build-arg PHP_VERSION=$v .
+  docker build -t asup:dev-$v --build-arg PHP_VERSION=$v . -f $DOCKERFILE
 done
--- build.sh
+++ build.sh
@@ -6,19 +6,20 @@ GIT_AUTO_MERGE=0
 
 CONSUMER="Aperta-Cloud-Build"
 
-GIT_BRANCH_TARGET="test-branch-kevin"
+GIT_BRANCH_TARGET="develop"
 COMPOSER_UPDATE_CONSTRAINTS=1
 VERBOSE=1
 SELF_TEST=1
+SKIP_AFTER_SELF_TEST=1
 DRY_RUN=0
 APP_PUBLIC_ROOT_DIRECTORY="web"
 
 VCS_PROVIDER="github"
 
-GIT_CLONE_URL="git@github.com:dazzletheweb/drupal-outdated-test.git"
+GIT_CLONE_URL="git@github.com:dazzletheweb/composer-outdated-project.git"
 GITHUB_HOST="https://github.com"
 GITHUB_OWNER="dazzletheweb"
-GITHUB_REPO="drupal-outdated-test"
+GITHUB_REPO="composer-outdated-project"
 GITHUB_ASUP_TOKEN=${GITHUB_ASUP_TOKEN}
 
 MATTERMOST_HOOK=${MATTERMOST_HOOK}
