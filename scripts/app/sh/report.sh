be0ded7cf91e51adbae1cd2b8e2105608a76dc94
Nicolas Stefanis <n.stefanis@gmail.com>
2023-06-12 14:57:35 +0300

    Silence mattermost hook call

--- scripts/app/sh/report.sh
+++ scripts/app/sh/report.sh
@@ -29,5 +29,7 @@ function report_mattermost() {
     OUTDATED_TABLE="${OUTDATED_TABLE} $(echo "${OUTDATED_FILE}" | sed -r 's/([a-z_-]+\/[a-z_-]+).* [v]?([0-9]+\.[0-9]+\.[0-9]+(-beta[0-9]+|-alpha[0-9]+|-dev[0-9]+)?).* [v]?([0-9]+\.[0-9]+\.[0-9]+(-beta[0-9]+|-alpha[0-9]+|-dev[0-9]+)?).*$/| \1 | \2 > \4 |/g')"
   fi
 
-  curl -i -X POST -H 'Content-Type: application/json' -d "{\"username\": \"ASUP\", \"text\": \"$REPORT $OUTDATED_TABLE\"}" "$MATTERMOST_HOOK"
+  # TODO: Check the response code and report if it is not 200
+  # curl -i --silent -o /dev/null -X POST -H 'Content-Type: application/json' -d "{\"username\": \"ASUP\", \"text\": \"$REPORT $OUTDATED_TABLE\"}" "$MATTERMOST_HOOK"
+  curl --silent -o /dev/null -X POST -H 'Content-Type: application/json' -d "{\"username\": \"ASUP\", \"text\": \"$REPORT $OUTDATED_TABLE\"}" "$MATTERMOST_HOOK"
 }
